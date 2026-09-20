aa20885c0da103fdd01266eee7399649997e8eec
alicia bactasa <bactasa.ak@gmail.com>
2026-09-20 16:14:14 +0800

    feat(assets): add static media assets, logos, and UI design mockups

---
+++ src/imports/Desktop1/index.tsx
@@ -0,0 +1,56 @@
+import svgPaths from "./svg-m1uyvhmyl3";
+
+export default function Desktop() {
+  return (
+    <div className="bg-white relative size-full" data-name="Desktop - 1">
+      <div className="absolute bg-[#b8d0ff] h-[640px] left-[32px] rounded-[45px] top-[253px] w-[1376px]" />
+      <div className="absolute bg-[#dfe9ff] h-[1205px] left-0 rounded-[45px] top-[940px] w-[1440px]" />
+      <div className="absolute bg-white h-[434px] left-[52px] rounded-[45px] top-[1016px] w-[669px]" />
+      <div className="absolute bg-white h-[528px] left-[52px] rounded-[45px] top-[1483px] w-[948px]" />
+      <div className="absolute bg-[#4869ff] h-[613px] left-[515px] rounded-[45px] top-[2295px] w-[853px]" />
+      <div className="absolute bg-white h-[528px] left-[1026px] rounded-[45px] top-[1483px] w-[342px]" />
+      <div className="absolute bg-white h-[391px] left-[572px] rounded-[45px] top-[2486px] w-[740px]" />
+      <div className="absolute bg-white h-[132px] left-[572px] rounded-[45px] top-[2329px] w-[740px]" />
+      <div className="absolute left-[593px] size-[100px] top-[2345px]">
+        <svg className="absolute block inset-0 size-full" fill="none" height="100" preserveAspectRatio="none" viewBox="0 0 100 100" width="100">
+          <circle cx="50" cy="50" fill="#4869FF" id="Ellipse 5" r="50" />
+        </svg>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[78px] leading-[103.856px] left-[642.5px] not-italic text-[60px] text-center text-white top-[2345px] w-[27px]">1</p>
+      <div className="absolute bg-white h-[544px] left-[-81px] rounded-[45px] top-[309px] w-[684px]" />
+      <div className="absolute bg-[#dfe9ff] h-[36px] left-[459px] rounded-tl-[45px] rounded-tr-[45px] top-[920px] w-[521px]" />
+      <div className="absolute h-[13px] left-[710px] top-[912px] w-[46px]">
+        <div className="absolute bottom-1/4 left-[6.7%] right-[6.7%] top-0">
+          <svg className="block size-full" fill="none" height="9.75" preserveAspectRatio="none" viewBox="0 0 39.8372 9.75" width="39.8372">
+            <path d={svgPaths.p10806700} fill="#DFE9FF" id="Polygon 1" />
+          </svg>
+        </div>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[109px] leading-[103.856px] left-[710px] not-italic text-[#302929] text-[98.91px] text-center top-[124px] tracking-[-2.4727px] w-[1378px]">Find Your Place In the Industry.</p>
+      <div className="absolute bg-[#0073ff] h-[78px] left-[32px] rounded-[45px] top-[14px] w-[1146px]" />
+      <div className="absolute bg-[#0073ff] h-[78px] left-[1191px] rounded-[45px] top-[14px] w-[217px]" />
+      <div className="absolute bg-white h-[71px] left-[410px] rounded-tl-[40px] rounded-tr-[40px] top-[21px] w-[173px]" />
+      <div className="absolute bg-[#0073ff] h-[67px] left-[420px] rounded-[30px] top-[25px] w-[153px]" />
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[924.5px] not-italic text-[24px] text-center text-white top-[6px] w-[99px]">FAQs</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[729px] not-italic text-[24px] text-center text-white top-[6px] w-[212px]">About</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[497px] not-italic text-[24px] text-center text-white top-[6px] w-[212px]">Home</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[81px] leading-[103.856px] left-[1301.5px] not-italic text-[24px] text-center text-white top-0 w-[193px]">Log In</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] leading-[0] left-[136.5px] not-italic text-[#232121] text-[40px] text-center top-0 tracking-[-2.4727px] whitespace-nowrap">
+        <span className="leading-[103.856px] text-white">Get</span>
+        <span className="leading-[103.856px] text-white">Hired</span>
+      </p>
+      <div className="absolute left-[1343px] size-[50px] top-[2577px]">
+        <svg className="absolute block inset-0 size-full" fill="none" height="50" preserveAspectRatio="none" viewBox="0 0 50 50" width="50">
+          <circle cx="25" cy="25" fill="#F3FAFF" fillOpacity="0.952941" id="Ellipse 2" r="25" />
+        </svg>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[1368px] not-italic text-[#0c0000] text-[24px] text-center top-[2551px] w-[212px]">{`>`}</p>
+      <div className="absolute left-[490px] size-[50px] top-[2577px]">
+        <svg className="absolute block inset-0 size-full" fill="none" height="50" preserveAspectRatio="none" viewBox="0 0 50 50" width="50">
+          <circle cx="25" cy="25" fill="#F3FAFF" id="Ellipse 3" r="25" />
+        </svg>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[515px] not-italic text-[#0c0000] text-[24px] text-center top-[2553px] w-[212px]">{`<`}</p>
+    </div>
+  );
+}
---
+++ src/imports/Desktop6/index.tsx
@@ -0,0 +1,206 @@
+import svgPaths from "./svg-vxa9w0z3l6";
+import imgImage1 from "./79306b04fdc367a311acb0dab8046e9dcc8a54f9.png";
+
+function Img() {
+  return (
+    <div className="absolute bg-white h-[118px] left-[331px] overflow-clip rounded-[16px] top-[265px] w-[227px]" data-name="img">
+      <div className="absolute inset-[-88px_0_-64.25px_0]" data-name="image 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage1.src} />
+      </div>
+    </div>
+  );
+}
+
+function Img1() {
+  return (
+    <div className="absolute bg-white h-[118px] left-[331px] overflow-clip rounded-[16px] top-[268px] w-[293px]" data-name="img">
+      <div className="absolute inset-[-88px_0_-64.25px_0]" data-name="image 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage1.src} />
+      </div>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div className="absolute left-[76px] size-[16px] top-[266px]" data-name="search 1">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g clipPath="url(#clip0_0_9)" id="search 1">
+          <path d={svgPaths.p2de17480} fill="white" id="Vector" />
+        </g>
+        <defs>
+          <clipPath id="clip0_0_9">
+            <rect fill="white" height="16" width="16" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Search1() {
+  return (
+    <div className="absolute left-[107px] size-[16px] top-[268px]" data-name="search 1">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g clipPath="url(#clip0_0_7)" id="search 1">
+          <path d={svgPaths.p2de17480} fill="black" id="Vector" />
+        </g>
+        <defs>
+          <clipPath id="clip0_0_7">
+            <rect fill="white" height="16" width="16" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function ClipboardMinus() {
+  return (
+    <div className="absolute left-[76px] size-[16px] top-[365px]" data-name="clipboard-minus 1">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g id="clipboard-minus 1">
+          <path clipRule="evenodd" d={svgPaths.p36d074d2} fill="white" fillRule="evenodd" id="Vector" />
+          <path d={svgPaths.p109ed400} fill="white" id="Vector_2" />
+          <path d={svgPaths.p1b1a7b80} fill="white" id="Vector_3" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function House1() {
+  return (
+    <div className="absolute left-[195px] size-[16px] top-[342px]" data-name="house 2">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g id="house 2">
+          <path d={svgPaths.p7a03240} fill="black" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function House() {
+  return (
+    <div className="absolute left-[90px] size-[16px] top-[159px]" data-name="house 1">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g id="house 1">
+          <path d={svgPaths.p7a03240} fill="white" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default function Desktop() {
+  return (
+    <div className="bg-white relative size-full" data-name="Desktop - 6">
+      <div className="absolute flex h-[987px] items-center justify-center left-[20px] top-[14px] w-[198px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#4869ff] h-[987px] relative rounded-[45px] w-[198px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[987px] items-center justify-center left-[20px] top-[14px] w-[225px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#4869ff] h-[987px] relative rounded-[45px] w-[225px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[278px] items-center justify-center left-[312px] top-[244px] w-[334px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[278px] relative rounded-[45px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] w-[334px]" />
+        </div>
+      </div>
+      <Img />
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[80px] leading-[0] left-[139.5px] not-italic text-[#232121] text-[0px] text-center top-[-8px] tracking-[-2.4727px] w-[103px]">
+        <span className="leading-[103.856px] text-[30px] text-white">Get</span>
+        <span className="leading-[103.856px] text-[#e5eaff] text-[30px]">Hired</span>
+      </p>
+      <div className="absolute flex h-[68px] items-center justify-center left-[1232px] top-[19px] w-[194px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[68px] relative rounded-[45px] shadow-[4px_8px_20px_0px_rgba(0,0,0,0.25)] w-[194px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[80px] items-center justify-center left-[27px] top-[125px] w-[218px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[80px] relative rounded-br-[45px] rounded-tr-[45px] w-[218px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[69px] items-center justify-center left-[34px] top-[130px] w-[211px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#4869ff] h-[69px] relative rounded-[45px] w-[211px]" />
+        </div>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[139.5px] not-italic text-[20px] text-center text-white top-[115px] w-[135px]">Home</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[139.5px] not-italic text-[20px] text-center text-white top-[223px] w-[135px]">Explore</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[64px] leading-[103.856px] left-[1307.5px] not-italic text-[#171212] text-[20px] text-center top-0 w-[135px]">User</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[64px] leading-[103.856px] left-[1307.5px] not-italic text-[#171212] text-[20px] text-center top-0 w-[135px]">User</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[64px] leading-[103.856px] left-[475px] not-italic text-[#171212] text-[20px] text-center top-[93px] w-[342px]">view your saved balh blah</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[139.5px] not-italic text-[20px] text-center text-white top-[322px] w-[135px]">Interview</p>
+      <div className="absolute flex h-[68px] items-center justify-center left-[1232px] top-[19px] w-[194px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[68px] relative rounded-[45px] shadow-[4px_8px_20px_0px_rgba(0,0,0,0.25)] w-[194px]" />
+        </div>
+      </div>
+      <div className="absolute h-[58px] left-[1358px] top-[23px] w-[59px]">
+        <svg className="absolute block inset-0 size-full" fill="none" height="58" preserveAspectRatio="none" viewBox="0 0 59 58" width="59">
+          <ellipse cx="29.5" cy="29" fill="#5462FF" id="Ellipse 4" rx="29.5" ry="29" />
+        </svg>
+      </div>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[149px] leading-[103.856px] left-[507px] not-italic text-[#302929] text-[60px] text-center top-[32px] tracking-[-2.4727px] w-[826px]">Welcome, User!</p>
+      <div className="absolute flex h-[987px] items-center justify-center left-[20px] top-[14px] w-[198px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#4869ff] h-[987px] relative rounded-[45px] w-[198px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[987px] items-center justify-center left-[20px] top-[14px] w-[225px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#0073ff] h-[987px] relative rounded-[45px] w-[225px]" />
+        </div>
+      </div>
+      <Img1 />
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[80px] leading-[0] left-[139.5px] not-italic text-[#232121] text-[0px] text-center top-[-8px] tracking-[-2.4727px] w-[103px]">
+        <span className="leading-[103.856px] text-[30px] text-white">Get</span>
+        <span className="leading-[103.856px] text-[#e5eaff] text-[30px]">Hired</span>
+      </p>
+      <div className="absolute flex h-[80px] items-center justify-center left-[27px] top-[125px] w-[218px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[80px] relative rounded-br-[45px] rounded-tr-[45px] w-[218px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[69px] items-center justify-center left-[34px] top-[130px] w-[211px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-[#0073ff] h-[69px] relative rounded-[45px] w-[211px]" />
+        </div>
+      </div>
+      <div className="absolute flex h-[69px] items-center justify-center left-[27px] top-[903px] w-[211px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="bg-white h-[69px] relative rounded-[45px] w-[211px]" />
+        </div>
+      </div>
+      <Search />
+      <Search1 />
+      <ClipboardMinus />
+      <House1 />
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[147.5px] not-italic text-[18px] text-center text-white top-[116px] w-[135px]">Home</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[135.5px] not-italic text-[18px] text-center text-white top-[223px] w-[135px]">Explore</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[64px] leading-[103.856px] left-[475px] not-italic text-[#171212] text-[20px] text-center top-[93px] w-[342px]">view your saved balh blah</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[25px] leading-[10px] left-[411px] not-italic text-[#171212] text-[12px] text-center top-[437px] w-[160px]">view your saved balh blah</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[143.5px] not-italic text-[18px] text-center text-white top-[322px] w-[135px]">Interview</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular',sans-serif] h-[81px] leading-[103.856px] left-[132.5px] not-italic text-[18px] text-black text-center top-[891px] w-[135px]">Log out</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Display:Regular',sans-serif] h-[149px] leading-[103.856px] left-[507px] not-italic text-[#302929] text-[60px] text-center top-[32px] tracking-[-2.4727px] w-[826px]">Welcome, User!</p>
+      <House />
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['DM_Serif_Text:Regular',sans-serif] h-[19px] leading-[10px] left-[428px] not-italic text-[#232323] text-[20px] text-center top-[403px] w-[212px]">COMPANY FULL NAME</p>
+      <p className="-translate-x-1/2 [word-break:break-word] absolute font-['Poppins:Regular','Noto_Sans:Regular','Noto_Sans_Math:Regular','Noto_Sans_Symbols:Regular','Noto_Sans_Symbols2:Regular',sans-serif] h-[19px] leading-[10px] left-[581px] text-[#232323] text-[10px] text-center top-[498px] w-[106px]" style={{ fontVariationSettings: '"CTGR" 0, "wdth" 100, "wght" 400' }}>
+        know more →
+      </p>
+      <div className="absolute h-0 left-[334.2px] top-[426.95px] w-[223.816px]">
+        <div className="absolute inset-[-0.2px_0_0_0]">
+          <svg className="block size-full" fill="none" height="0.2" preserveAspectRatio="none" viewBox="0 0 223.816 0.2" width="223.816">
+            <line id="Line 1" stroke="#00064A" strokeWidth="0.2" x2="223.816" y1="0.1" y2="0.1" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
